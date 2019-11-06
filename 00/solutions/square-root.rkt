9b9d0637e9bb246e8f7ee8902806aac29c27b5ad
Viktorsmg <Viktor.smg@gmail.com>
2019-11-06 23:32:09 +0300

    Add forgotten sqrt helper functions
    I forgot to add a few helper functions for the sqrt
    I've added them, as well as made the precision argument optional

--- 00/solutions/square-root.rkt
+++ 00/solutions/square-root.rkt
@@ -6,17 +6,33 @@
 ;Дигаме го с +1 докато не надминем търсеният квадрат
 ;Намерили сме прецизност до тази цифра, започваме да дигаме с +0.1 докато не го надминем пак
 ;... +0.01... +0.001...
-(define (my-sqrt x precision)
-    (define (approx curr digiti depth)
-        (if ( or (> depth precision) (equal? (sq curr) x) )
+
+(define (sq x) (* x x))
+
+(define (add1_dig n dig)
+    (+ n (/ 1 (expt 10 dig)))
+)
+
+(define (my-sqrt x . precision)
+    (define (approx curr digiti depth limit)
+        (if ( or (> depth limit) (equal? (sq curr) x) )
             curr
             (if (> (sq (add1_dig curr digiti)) x)
-                (approx curr (+ digiti 1) (+ depth 1))
-                (approx (add1_dig curr digiti) digiti depth)
+                (approx curr (+ digiti 1) (+ depth 1) limit)
+                (approx (add1_dig curr digiti) digiti depth limit)
             )
         )
     )
-    (+ (approx 0 0 0) 0.0)
-)
 
-(define (my-sqrt x) (my-sqrt x 10))
+    (if (null? precision)
+        (+ (approx 0 0 0 5) 0.0)
+        (if (number? precision)
+            (+ (approx 0 0 0 precision) 0.0)
+            (if (number? (car precision))
+                (+ (approx 0 0 0 (car precision)) 0.0)
+                (+ (approx 0 0 0 5) 0.0)
+            )
+        )
+    )
+    
+)
